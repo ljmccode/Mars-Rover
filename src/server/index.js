95c256c8380443476d3f97e2b38105e484b99f06
Laura <lmcsay@gmail.com>
2021-01-06 17:13:23 -0600

    Feat: Add rover radio

--- src/server/index.js
+++ src/server/index.js
@@ -1,26 +1,34 @@
-require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const fetch = require('node-fetch')
 const path = require('path')
+require('dotenv').config({path: path.resolve(__dirname, '../../.env')})
+
 
 const app = express()
 const port = 3000
+
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use('/', express.static(path.join(__dirname, '../public')))
 
+
 // your API calls
+
+// want to show most recent images taken of that rover
 
 // example API call
 app.get('/apod', async (req, res) => {
+    
     try {
         let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
             .then(res => res.json())
+        // sending image json to the /apod route
         res.send({ image })
     } catch (err) {
+        console.log(process.env.API_KEY)
         console.log('error:', err);
     }
 })
