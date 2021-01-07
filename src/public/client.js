75ec5d3674ebbebdee86ce73a197ffc4d5ecb2ba
Laura <lmcsay@gmail.com>
2021-01-07 13:53:06 -0600

    Feat: Render info on button click

--- src/public/client.js
+++ src/public/client.js
@@ -50,21 +50,11 @@ const App = (state) => {
     return `
         <header></header>
         <main>
-            ${Greeting(store.user.name)}
+            <h1>Mars Rover</h1>
             <section>
-                <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
-                <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
-                </p>
-                ${ImageOfTheDay(apod)}
+                <h3>Welcome to the Mars Rover page!</h3>
+                ${curiosityInfo(curiosity)}
             </section>
-            ${curiosityInfo(curiosity)}
         </main>
         <footer></footer>
     `
@@ -79,71 +69,74 @@ window.addEventListener('load', () => {
 // ------------------------------------------------------  COMPONENTS
 
 // Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
-const Greeting = (name) => {
-    if (name) {
-        return `
-            <h1>Welcome, ${name}!</h1>
-        `
-    }
+// const Greeting = (name) => {
+//     if (name) {
+//         return `
+//             <h1>Welcome, ${name}!</h1>
+//         `
+//     }
 
-    return `
-        <h1>Hello!</h1>
-    `
-}
+//     return `
+//         <h1>Hello!</h1>
+//     `
+// }
 
 // Example of a pure function that renders infomation requested from the backend
-const ImageOfTheDay = (apod) => {
-    // console.log('apod: ', apod);
+// const ImageOfTheDay = (apod) => {
+//     // console.log('apod: ', apod);
 
-    // If image does not already exist, or it is not from today -- request it again
-    const today = new Date()
-    const photodate = new Date(apod.date)
-    // console.log(photodate.getDate(), today.getDate());
+//     // If image does not already exist, or it is not from today -- request it again
+//     const today = new Date()
+//     const photodate = new Date(apod.date)
+//     // console.log(photodate.getDate(), today.getDate());
 
-    // console.log(photodate.getDate() === today.getDate());
-    if (!apod || apod.date === today.getDate() ) {
-        getImageOfTheDay(store)
-    }
+//     // console.log(photodate.getDate() === today.getDate());
+//     if (!apod || apod.date === today.getDate() ) {
+//         getImageOfTheDay(store)
+//     }
 
-    // check if the photo of the day is actually type video!
-    if (apod.media_type === "video") {
-        return (`
-            <p>See today's featured video <a href="${apod.url}">here</a></p>
-            <p>${apod.title}</p>
-            <p>${apod.explanation}</p>
-        `)
-    } else {
-        return (`
-            <img src="${apod.image.url}" height="350px" width="100%" />
-            <p>${apod.image.explanation}</p>
-        `)
-    }
-}
+//     // check if the photo of the day is actually type video!
+//     if (apod.media_type === "video") {
+//         return (`
+//             <p>See today's featured video <a href="${apod.url}">here</a></p>
+//             <p>${apod.title}</p>
+//             <p>${apod.explanation}</p>
+//         `)
+//     } else {
+//         return (`
+//             <img src="${apod.image.url}" height="350px" width="100%" />
+//             <p>${apod.image.explanation}</p>
+//         `)
+//     }
+// }
 
 const curiosityInfo = (curiosity) => {
     if (!curiosity) {
-        curiousityObj(store);
-    }
-    return (`
+        return (`
+            <p>Select a rover to see the latest pictures</p>
+        `)
+    } else {
+        return (`
         <img src="${curiosity.curiosity.photos[0].img_src}" height="350px" width="auto" />
         <p>${curiosity.curiosity.photos[0].camera.full_name}</p>
     `)
+    }
 }
 
 // ------------------------------------------------------  API CALLS
 
 // Example API call
 
-const getImageOfTheDay = (state) => {
-    let { apod } = state
+// const getImageOfTheDay = (state) => {
+//     let { apod } = state
 
-    fetch(`http://localhost:3000/apod`)
-        .then(res => res.json())
-        // apod is the data being sent from the app.get in index
-        .then(apod => updateStore(store, { apod }))
+//     fetch(`http://localhost:3000/apod`)
+//         .then(res => res.json())
+//         // apod is the data being sent from the app.get in index
+//         .then(apod => updateStore(store, { apod }))
 
-    return apod
-}
+//     return apod
+// }
 
 const curiousityObj = (state) => {
     let { curiosity } = state
@@ -152,6 +145,6 @@ const curiousityObj = (state) => {
         .then(res => res.json())
         // curiosity is the data being sent from the app.get in index
         .then(curiosity => updateStore(store, { curiosity }))
-        
+
     return curiosity
 }
